1f1545564c48739cb068539e485d26d3350165cf
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-01 18:17:45 -0300

    feat: migration of blog_posts

--- src/migrations/20221201152015-blog_posts.js
+++ src/migrations/20221201152015-blog_posts.js
@@ -2,20 +2,44 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+
+    await queryInterface.createTable('blog_posts',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type:Sequelize.INTEGER
+        },
+        title: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        content: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          },
+        userId: {
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+          onDelete: 'CASCADE',
+        }, 
+          published:  {
+            allowNull: false,
+            type: Sequelize.DATE
+          },
+        update: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+      });
+
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+     await queryInterface.dropTable('blog_posts');
   }
 };
