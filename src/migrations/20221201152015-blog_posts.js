08ee76480d39224524019c53d9cb6cefe0a399e2
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-01 21:52:59 -0300

    fix: migrations of blog_posts

--- src/migrations/20221201152015-blog_posts.js
+++ src/migrations/20221201152015-blog_posts.js
@@ -30,7 +30,7 @@ module.exports = {
             allowNull: false,
             type: Sequelize.DATE
           },
-        update: {
+        updated: {
           allowNull: false,
           type: Sequelize.DATE
         },
