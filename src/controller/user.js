e2612f7be618bb1fce7e2c62aa8bb3bfd184b28c
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-07 12:00:40 -0300

    fix: bug on controller user

--- src/controller/user.js
+++ src/controller/user.js
@@ -15,7 +15,7 @@ const getAlluser = async (req, res) => {
 
 const userbyId = async (req, res) => {
     const { id } = req.params;
-    const { user } = await userService.userbyId(id);
+    const user = await userService.userbyId(id);
     res.status(200).json(user);
 };
 
