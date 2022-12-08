841a177ec712ba492ed6d62f9becd2143c9de10a
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-08 19:47:20 -0300

    remove trash login files

--- src/controller/login.js
+++ src/controller/login.js
@@ -1,8 +1,10 @@
 const loginService = require('../service/login');
+const { createToken } = require('../auth/jwtFunctions');
 
 const login = async (req, res) => {
     const { email } = req.body;
-    const { token } = await loginService.login(email);
+    const { userWithoutPassword } = await loginService.login(email);
+    const token = createToken(userWithoutPassword);
     res.status(200).json({ token });
 };
 
--- src/service/login.js
+++ src/service/login.js
@@ -1,13 +1,9 @@
 const { User } = require('../models');
-const { createToken } = require('../auth/jwtFunctions');
 
 const login = async (email) => {
-    const user = await User.findOne({ where: { email } });
- 
-    const { password: _, ...userWithoutPassword } = user.dataValues;
-    const token = createToken(userWithoutPassword);
-    
-    return { token };
+    const { dataValues } = await User.findOne({ where: { email } });
+    const { password: _, ...userWithoutPassword } = dataValues;
+    return { userWithoutPassword };
 };
 
 module.exports = {
