31594678a0b2d3e225f05dfe4d6e9332b325eee4
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-10 14:01:52 -0300

    refactor: change post midd to joi

--- src/middlewares/post.js
+++ src/middlewares/post.js
@@ -1,3 +1,5 @@
+const Joi = require('joi');
+
 const requiredFields = (req, res, next) => {
     const { title, content, categoryIds } = req.body;
     if (!title || !content || !categoryIds) {
@@ -5,11 +7,16 @@ const requiredFields = (req, res, next) => {
     } next();
 };
 
-const requiredFieldsUpdatePost = (req, res, next) => {
-    const { title, content } = req.body;
-    if (!title || !content) {
-       return res.status(400).json({ message: 'Some required fields are missing' });
-    } next();
+const requiredFieldsUpdatePost = async (req, res, next) => {
+    const { error } = Joi.object({
+        title: Joi.string().required(),
+        content: Joi.string().required(),
+    }).validate(req.body);
+
+    if (error) {
+        return res.status(400).json({ message: 'Some required fields are missing' });
+    }
+    next();
 };
 
 module.exports = {
