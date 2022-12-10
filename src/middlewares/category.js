49c0c2de302c44a88b7a9b07fdb035e5d95e2a07
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-10 13:47:10 -0300

    refactor: middlewares category to joi type

--- src/middlewares/category.js
+++ src/middlewares/category.js
@@ -1,8 +1,15 @@
+const Joi = require('joi');
+
 const nameRequired = (req, res, next) => {
-    const { name } = req.body; 
-    if (!name) {
-        return res.status(400).json({ message: '"name" is required' });
-    } next();
+    const { name } = req.body;
+    const schema = Joi.object({
+        name: Joi.string().required(),
+    });
+    const { error } = schema.validate({ name });
+    if (error) {
+        return res.status(400).json({ message: error.details[0].message });
+    }
+    next();
 };
 
 module.exports = {
